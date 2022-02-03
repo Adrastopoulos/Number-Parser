d3407662994ed9a7978181e63330b04c23de1abe
Adrastopoulos <76796897+Adrastopoulos@users.noreply.github.com>
2022-02-02 17:10:36 -0800

    fix: default param implementation

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,3 @@
 export function parseString(string: string): number;
-export function parseNumber(number: number, decimals: number = 2): string;
+export function parseNumber(number: number, decimals?: number): string;
 export function parseInteger(string: string): number;
